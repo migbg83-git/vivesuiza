6ad4fe85650b358177766a9919eef8b356fd7d69
Miguel Benito Garcia <miguel.benito.garcia@gmail.com>
2026-06-17 09:28:12 +0200

    Improve home page with categories

---
+++ src/app/core/constants/categories.ts
@@ -0,0 +1,32 @@
+export const CATEGORIES = [
+    {
+        slug: 'antes-de-mudarse',
+        name: 'Antes de mudarse',
+        description: 'Preparativos, permisos y primeros pasos antes de venir.'
+    },
+    {
+        slug: 'trabajo',
+        name: 'Trabajo',
+        description: 'Empleo, CV, entrevistas, salarios y sectores.'
+    },
+    {
+        slug: 'vivienda',
+        name: 'Vivienda',
+        description: 'Alquileres, barrios, contratos y búsqueda de piso.'
+    },
+    {
+        slug: 'seguros',
+        name: 'Seguros',
+        description: 'Seguro médico, franquicias y comparativas.'
+    },
+    {
+        slug: 'impuestos',
+        name: 'Impuestos',
+        description: 'Fiscalidad, impuesto en origen y declaración.'
+    },
+    {
+        slug: 'vida-diaria',
+        name: 'Vida diaria',
+        description: 'Transporte, compras, idioma e integración.'
+    },
+];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { ContentService } from '../../core/services/content.service';
 import { RouterLink } from '@angular/router';
+import { CATEGORIES } from '../../core/constants/categories';
 
 @Component({
   selector: 'app-home',
@@ -13,4 +14,5 @@ export class Home {
   private readonly contentService = inject(ContentService);
 
   readonly articles$ = this.contentService.getArticles();
+  readonly categories = CATEGORIES;
 }
